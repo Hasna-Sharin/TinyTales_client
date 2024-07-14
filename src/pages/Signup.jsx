320cb3db302d1feb58b4e326c29212af0dcb79c7
Hasna-Sharin <sharinhasna29@gmail.com>
2024-07-14 10:03:36 +0530

    again signUp modified

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -131,7 +131,7 @@ const SignUp = () => {
                             <button type="submit" className="w-full text-white  bg-black hover:bg-black/75 focus:ring-black/75  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create an account</button>
                             <div className='flex justify-center'>
 
-                            <GoogleLogin   onSuccess={handleGoogleLogin} onError={() => {console.log('Login Failed');}}/>;
+                            <GoogleLogin   onSuccess={handleGoogleLogin} onError={() => {console.log('Login Failed');}}/>
                             </div>
                             
                             <Link to={"/"}>
